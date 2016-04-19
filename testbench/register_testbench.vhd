98945b1a541c4531bb7d260952740bb87e7abbfe
root <paulormnas@gmail.com>
2016-04-19 00:04:44 -0300

    Bug fix on register read/write

--- testbench/register_testbench.vhd
+++ testbench/register_testbench.vhd
@@ -1,6 +1,7 @@
 -- Code your testbench here
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity tb_reg is
 end tb_reg;
@@ -18,21 +19,47 @@ architecture behav of tb_reg is
            ReadData2 : out STD_LOGIC_VECTOR (31 downto 0));
 	end component;
     
-    signal t_CLK, t_RegWrite : STD_LOGIC;
+    signal t_CLK : STD_LOGIC := '0';
+    signal t_RegWrite : STD_LOGIC;
     signal t_ReadAddrs1, t_ReadAddrs2, t_WriteAddrs : STD_LOGIC_VECTOR (4 downto 0);
-    
+    signal temp : STD_LOGIC_VECTOR (4 downto 0) := "00000";
     signal t_WriteData, t_ReadData1, t_ReadData2 : STD_LOGIC_VECTOR (31 downto 0);
     
 begin
-    process
+    clk_control : process
     begin
+        
+        for i in 0 to 31 loop
+            wait for 1 ns;
+            
+            t_CLK <= '1';
+            t_RegWrite <= '1';
+            if (temp = "00000") then
+                t_ReadAddrs1 <= "00000";
+                t_ReadAddrs2 <= "00000";
+            else
+                t_ReadAddrs1 <= temp;
+                t_ReadAddrs2 <= std_logic_vector(unsigned(temp) - "0001");
+            end if;
+            t_WriteAddrs <= temp;
+            t_WriteData <= x"00000000";
+            temp <= std_logic_vector(unsigned(temp) + "0001");
+            
+            wait for 1 ns;
+            
+            t_CLK <= '0';
+        
+        end loop;
+        
+        wait for 1 ns;
+        
     	t_CLK <= '1';
         t_RegWrite <= '1';
         t_ReadAddrs1 <= "00000";
         t_ReadAddrs2 <= "00000";
         t_WriteAddrs <= "00001";
         t_WriteData <= x"000000F0";
-        
+
         wait for 1 ns;
         
         t_CLK <= '0';
@@ -40,16 +67,32 @@ begin
         wait for 1 ns;
         
         t_CLK <= '1';
-        t_RegWrite <= '0';
+        t_RegWrite <= '1';
         t_ReadAddrs1 <= "00001";
         t_ReadAddrs2 <= "00000";
         t_WriteAddrs <= "00010";
         t_WriteData <= x"000000F0";
-
-		wait for 1 ns;
         
+        wait for 1 ns;
+                
+        t_CLK <= '0';
+		
+		wait for 1 ns;
+                
+        t_CLK <= '1';
+        t_RegWrite <= '0';
+        t_ReadAddrs1 <= "00001";
+        t_ReadAddrs2 <= "00010";
+        t_WriteAddrs <= "00001";
+        t_WriteData <= x"000000F0";
+        
+        wait for 1 ns;
+               
+        t_CLK <= '0';
+                
+        wait for 1 ns;
 		wait;
-    end process;
+    end process clk_control;
     
     REG1 : Register_v2 port map (t_CLK, t_RegWrite, t_ReadAddrs1, t_ReadAddrs2, t_WriteAddrs, t_WriteData, t_ReadData1, t_ReadData2);
     
