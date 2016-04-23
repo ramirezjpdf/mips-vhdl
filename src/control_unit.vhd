75f42724f3b512243b451fab7d3ce6e0a90aa285
ramirezjpdf <ramirez.jpdf@ymail.com>
2016-04-23 11:59:17 -0300

    fix some compilation bugs in control unit

--- src/control_unit.vhd
+++ src/control_unit.vhd
@@ -1,8 +1,8 @@
-library ieeeç;
+library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-use work.const.all
+use work.const.all;
 
 entity control_unit is
     port(
@@ -30,25 +30,24 @@ architecture behav of control_unit is
 subtype state is integer range 0 to 12;
 
 -- GENERAL STATES
-constant INSTRUCTION_FETCH    : state := 0;
-constant INST_DECODE_REG_READ : state := 1;
--- LW OR SW STATES
-constant MEM_ADDR_COMP        : state := 2;
-constant LW_MEM_ACCES         : state := 3;
-constant MEM_READ_COMPLETION  : state := 4;
-constant SW_MEM_ACCES         : state := 5;
+constant INSTRUCTION_FETCH       : state := 0;
+constant INST_DECODE_REG_READ    : state := 1;
+-- LW OR SW AND ADDI STATES 
+constant MEM_ADDR_COMP_ADDI_EXEC : state := 2;
+constant LW_MEM_ACCESS            : state := 3;
+constant MEM_READ_COMPLETION     : state := 4;
+constant SW_MEM_ACCESS            : state := 5;
+constant ADDI_COMPLETION         : state := 12;
 -- R-TYPE ARITH AND LOGIC STATES
-constant EXECUTION            : state := 6;
-constant R_TYPE_COMPLETION    : state := 7;
+constant EXECUTION               : state := 6;
+constant R_TYPE_COMPLETION       : state := 7;
 -- BRANCH STATES
-constant BRANCH_COMPLETION    : state := 8;
+constant BRANCH_COMPLETION       : state := 8;
 -- JUMP STATES
-constant JUMP_COMPLETION      : state := 9;
-constant JAL_COMPLETION       : state := 10;
-constant JR_COMPLETION        : state := 11;
--- ADDI STATES
-constant ADDI_EXECUTION       : state := MEM_ADDR_COMP;
-constant ADDI_COMPLETION      : state := 12;
+constant JUMP_COMPLETION         : state := 9;
+constant JAL_COMPLETION          : state := 10;
+constant JR_COMPLETION           : state := 11;
+
 
 -- CURRENT_STATE
 signal current_state : state := INSTRUCTION_FETCH;
@@ -63,8 +62,8 @@ begin
                     current_state <= INST_DECODE_REG_READ;
                 when INST_DECODE_REG_READ =>
                     case op_code is
-                        when LW | SW =>
-                            current_state <= MEM_ADDR_COMP;
+                        when LW | SW | ADDI=>
+                            current_state <= MEM_ADDR_COMP_ADDI_EXEC;
                         when R_TYPE =>
                             current_state <= EXECUTION;
                         when BEQ | BNE =>
@@ -73,30 +72,25 @@ begin
                             current_state <= JUMP_COMPLETION;
                         when JAL =>
                             current_state <= JAL_COMPLETION;
-                        when ADDI =>
-                            current_state <= ADDI_EXECUTION;
                         when others =>
                             current_state <= INSTRUCTION_FETCH;
                     end case;
 
-                -- LW OR SW STATES
-                -- this state is the same for addi and lw or sw
-                when MEM_ADDR_COMP | ADDI_EXECUTION =>
+                -- LW OR SW AND ADDI STATES
+                when MEM_ADDR_COMP_ADDI_EXEC =>
                     case op_code is
                         when LW =>
-                            current_state <= LW_MEM_ACCES;
+                            current_state <= LW_MEM_ACCESS;
                         when SW =>
-                            current_state <= SW_MEM_ACCES;
+                            current_state <= SW_MEM_ACCESS;
                         when ADDI =>
-                            current_state <= ADDI_COMPLETION
+                            current_state <= ADDI_COMPLETION;
                         when others =>
                             current_state <= INSTRUCTION_FETCH;
                     end case;
-                when LW_MEM_ACCES =>
+                when LW_MEM_ACCESS =>
                     current_state <= MEM_READ_COMPLETION;
-                when SW_MEM_ACCES =>
-                    current_state <= INSTRUCTION_FETCH;
-                when MEM_READ_COMPLETION =>
+                when SW_MEM_ACCESS | MEM_READ_COMPLETION | ADDI_COMPLETION =>
                     current_state <= INSTRUCTION_FETCH;
 
                 -- R-TYPE ARITH AND LOGIC STATES
@@ -113,16 +107,12 @@ begin
                 when JUMP_COMPLETION | JAL_COMPLETION =>
                     current_state <= INSTRUCTION_FETCH;
 
-                -- ADDI STATES
-                when ADDI_COMPLETION =>
-                    current_state <= INSTRUCTION_FETCH;
-
                 -- OTHERS
                 when others =>
                     current_state <= INSTRUCTION_FETCH;
             end case;
         end if;
-    end next_state_function;
+    end process;
 
     output_function : process(current_state)
     begin
@@ -157,8 +147,8 @@ begin
                 alu_src_b     <= BRANCH_ADDR_ALU_SRC_B;
                 pc_source     <= ALU_RESULT_PC_SOURCE;
 
-            -- LW OR SW STATES        =>
-            when MEM_ADDR_COMP        =>
+            -- LW OR SW AND ADDI  STATES
+            when MEM_ADDR_COMP_ADDI_EXEC =>
                 pc_write_cond <= DEASSERTED; 
                 pc_write      <= DEASSERTED;
                 i_or_d        <= DEASSERTED;
@@ -172,7 +162,7 @@ begin
                 alu_src_a     <= ASSERTED;
                 alu_src_b     <= IMMED_ALU_SRC_B;
                 pc_source     <= ALU_RESULT_PC_SOURCE;
-            when LW_MEM_ACCES         =>
+            when LW_MEM_ACCESS         =>
                 pc_write_cond <= DEASSERTED; 
                 pc_write      <= DEASSERTED;
                 i_or_d        <= ASSERTED;
@@ -200,7 +190,7 @@ begin
                 alu_src_a     <= DEASSERTED;
                 alu_src_b     <= B_ALU_SRC_B;
                 pc_source     <= ALU_RESULT_PC_SOURCE;
-            when SW_MEM_ACCES         =>
+            when SW_MEM_ACCESS         =>
                 pc_write_cond <= DEASSERTED; 
                 pc_write      <= DEASSERTED;
                 i_or_d        <= ASSERTED;
@@ -212,6 +202,20 @@ begin
                 reg_dst       <= RT_REG_DST;
                 alu_op        <= LW_OR_SW_ADD;
                 alu_src_a     <= DEASSERTED;
+                alu_src_b     <= B_ALU_SRC_B;
+                pc_source     <= ALU_RESULT_PC_SOURCE;
+            when ADDI_COMPLETION      =>
+                pc_write_cond <= DEASSERTED; 
+                pc_write      <= DEASSERTED;
+                i_or_d        <= DEASSERTED;
+                mem_read      <= DEASSERTED;
+                mem_write     <= DEASSERTED;
+                mem_to_reg    <= ALU_OUT_MEM_TO_REG;
+                ir_write      <= DEASSERTED;
+                reg_write     <= ASSERTED;
+                reg_dst       <= RT_REG_DST;
+                alu_op        <= LW_OR_SW_ADD;
+                alu_src_a     <= ASSERTED;
                 alu_src_b     <= B_ALU_SRC_B;
                 pc_source     <= ALU_RESULT_PC_SOURCE;
 
@@ -296,22 +300,6 @@ begin
             -- part of intruction to know how to
             -- properly signalize this instruction
 
-            -- ADDI STATES
-            when ADDI_COMPLETION      =>
-                pc_write_cond <= DEASSERTED; 
-                pc_write      <= DEASSERTED;
-                i_or_d        <= DEASSERTED;
-                mem_read      <= DEASSERTED;
-                mem_write     <= DEASSERTED;
-                mem_to_reg    <= ALU_OUT_MEM_TO_REG;
-                ir_write      <= DEASSERTED;
-                reg_write     <= ASSERTED;
-                reg_dst       <= RT_REG_DST;
-                alu_op        <= LW_OR_SW_ADD;
-                alu_src_a     <= ASSERTED;
-                alu_src_b     <= B_ALU_SRC_B;
-                pc_source     <= ALU_RESULT_PC_SOURCE;
-
             -- OTHERS
             when others      =>
                 pc_write_cond <= 'X'; 
@@ -329,6 +317,6 @@ begin
                 pc_source     <= "XX";
         end case;
     
-    end output_function;
+    end process;
 
 end behav;
