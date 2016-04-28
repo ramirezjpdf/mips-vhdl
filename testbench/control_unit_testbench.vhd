9d8c0fd9adb2ffdf52c685bb1832b4a67ec4cee0
ramirezjpdf <ramirez.jpdf@ymail.com>
2016-04-28 17:14:31 -0300

    fix compilation errors

--- testbench/control_unit_testbench.vhd
+++ testbench/control_unit_testbench.vhd
@@ -33,28 +33,60 @@ component control_unit is
     );
 end component;
 
-signal t_clk     : in std_logic;
-signal t_op_code : in std_logic_vector (5 downto 0);
+signal t_clk           : std_logic;
+signal t_op_code       : std_logic_vector (5 downto 0);
+
+signal t_pc_write_cond : std_logic;
+signal t_bne_cond      : std_logic;
+signal t_pc_write      : std_logic;
+signal t_i_or_d        : std_logic;
+signal t_mem_read      : std_logic;
+signal t_mem_write     : std_logic;
+signal t_mem_to_reg    : std_logic_vector (1 downto 0);
+signal t_ir_write      : std_logic;
+signal t_reg_write     : std_logic;
+signal t_reg_dst       : std_logic_vector (1 downto 0);
+signal t_alu_op        : std_logic_vector (1 downto 0);
+signal t_alu_src_a     : std_logic;
+signal t_alu_src_b     : std_logic_vector (1 downto 0);
+signal t_pc_source     : std_logic_vector (1 downto 0);
 
 begin
-
-    process(clk)
-    function assert_inst_fetch(
-        pc_write_cond : std_logic,
-        bne_cond      : std_logic,
-        pc_write      : std_logic,
-        i_or_d        : std_logic,
-        mem_read      : std_logic,
-        mem_write     : std_logic,
-        mem_to_reg    : std_logic_vector (1 downto 0),
-        ir_write      : std_logic,
-        reg_write     : std_logic,
-        reg_dst       : std_logic_vector (1 downto 0),
-        alu_op        : std_logic_vector (1 downto 0),
-        alu_src_a     : std_logic,
-        alu_src_b     : std_logic_vector (1 downto 0),
+    cun : control_unit port map (clk => t_clk,
+                                 op_code => t_op_code,
+                                 
+                                 pc_write_cond => t_pc_write_cond,
+                                 bne_cond      => t_bne_cond,
+                                 pc_write      => t_pc_write,
+                                 i_or_d        => t_i_or_d,
+                                 mem_read      => t_mem_read,
+                                 mem_write     => t_mem_write,
+                                 mem_to_reg    => t_mem_to_reg,
+                                 ir_write      => t_ir_write,
+                                 reg_write     => t_reg_write,
+                                 reg_dst       => t_reg_dst,
+                                 alu_op        => t_alu_op,
+                                 alu_src_a     => t_alu_src_a,
+                                 alu_src_b     => t_alu_src_b,
+                                 pc_source     => t_pc_source
+                                 );
+    process
+    procedure assert_inst_fetch(
+        pc_write_cond : std_logic;
+        bne_cond      : std_logic;
+        pc_write      : std_logic;
+        i_or_d        : std_logic;
+        mem_read      : std_logic;
+        mem_write     : std_logic;
+        mem_to_reg    : std_logic_vector (1 downto 0);
+        ir_write      : std_logic;
+        reg_write     : std_logic;
+        reg_dst       : std_logic_vector (1 downto 0);
+        alu_op        : std_logic_vector (1 downto 0);
+        alu_src_a     : std_logic;
+        alu_src_b     : std_logic_vector (1 downto 0);
         pc_source     : std_logic_vector (1 downto 0)
-    ) return boolean is
+    )  is
     begin
         assert pc_write_cond = DEASSERTED report "pc_write_cond Instruction fetch state error";
         assert bne_cond      = DEASSERTED report "bne_cond Instruction fetch state error";
@@ -71,24 +103,24 @@ begin
         assert alu_src_b     = FOUR_ALU_SRC_B report "alu_src_b Instruction fetch state error";
         assert pc_source     = ALU_RESULT_PC_SOURCE report "pc_source Instruction fetch state error";
         
-    end assert_inst_fetch;
+    end procedure assert_inst_fetch;
     
-    function assert_reg_read_inst_decode(
-        pc_write_cond : std_logic,
-        bne_cond      : std_logic,
-        pc_write      : std_logic,
-        i_or_d        : std_logic,
-        mem_read      : std_logic,
-        mem_write     : std_logic,
-        mem_to_reg    : std_logic_vector (1 downto 0),
-        ir_write      : std_logic,
-        reg_write     : std_logic,
-        reg_dst       : std_logic_vector (1 downto 0),
-        alu_op        : std_logic_vector (1 downto 0),
-        alu_src_a     : std_logic,
-        alu_src_b     : std_logic_vector (1 downto 0),
+    procedure assert_reg_read_inst_decode(
+        pc_write_cond : std_logic;
+        bne_cond      : std_logic;
+        pc_write      : std_logic;
+        i_or_d        : std_logic;
+        mem_read      : std_logic;
+        mem_write     : std_logic;
+        mem_to_reg    : std_logic_vector (1 downto 0);
+        ir_write      : std_logic;
+        reg_write     : std_logic;
+        reg_dst       : std_logic_vector (1 downto 0);
+        alu_op        : std_logic_vector (1 downto 0);
+        alu_src_a     : std_logic;
+        alu_src_b     : std_logic_vector (1 downto 0);
         pc_source     : std_logic_vector (1 downto 0)
-    ) return boolean is
+    )  is
     begin
         assert pc_write_cond = DEASSERTED report "pc_write_cond Instruction decode state error";
         assert bne_cond      = DEASSERTED report "bne_cond Instruction decode state error";
@@ -104,24 +136,24 @@ begin
         assert alu_src_a     = DEASSERTED report "alu_src_a Instruction decode state error";
         assert alu_src_b     = B_ALU_SRC_B report "alu_src_b Instruction decode state error";
         assert pc_source     = ALU_RESULT_PC_SOURCE report "pc_source Instruction decode state error";
-    end assert_reg_read_inst_decode;
+    end procedure assert_reg_read_inst_decode;
     
-    function assert_addr_calc
-        pc_write_cond : std_logic,
-        bne_cond      : std_logic,
-        pc_write      : std_logic,
-        i_or_d        : std_logic,
-        mem_read      : std_logic,
-        mem_write     : std_logic,
-        mem_to_reg    : std_logic_vector (1 downto 0),
-        ir_write      : std_logic,
-        reg_write     : std_logic,
-        reg_dst       : std_logic_vector (1 downto 0),
-        alu_op        : std_logic_vector (1 downto 0),
-        alu_src_a     : std_logic,
-        alu_src_b     : std_logic_vector (1 downto 0),
+    procedure assert_addr_calc(
+        pc_write_cond : std_logic;
+        bne_cond      : std_logic;
+        pc_write      : std_logic;
+        i_or_d        : std_logic;
+        mem_read      : std_logic;
+        mem_write     : std_logic;
+        mem_to_reg    : std_logic_vector (1 downto 0);
+        ir_write      : std_logic;
+        reg_write     : std_logic;
+        reg_dst       : std_logic_vector (1 downto 0);
+        alu_op        : std_logic_vector (1 downto 0);
+        alu_src_a     : std_logic;
+        alu_src_b     : std_logic_vector (1 downto 0);
         pc_source     : std_logic_vector (1 downto 0)
-    ) return boolean is
+    )  is
     begin
         assert pc_write_cond = DEASSERTED report "pc_write_cond  address calc state error";
         assert bne_cond      = DEASSERTED report "bne_cond       address calc state error";
@@ -137,24 +169,24 @@ begin
         assert alu_src_a     = ASSERTED report "alu_src_a      address calc state error";
         assert alu_src_b     = IMMED_ALU_SRC_B report "alu_src_b      address calc state error";
         assert pc_source     = ALU_RESULT_PC_SOURCE report "pc_source      address calc state error";
-    end assert_addr_calc;
+    end procedure assert_addr_calc;
     
-    function assert_mem_access
-        pc_write_cond : std_logic,
-        bne_cond      : std_logic,
-        pc_write      : std_logic,
-        i_or_d        : std_logic,
-        mem_read      : std_logic,
-        mem_write     : std_logic,
-        mem_to_reg    : std_logic_vector (1 downto 0),
-        ir_write      : std_logic,
-        reg_write     : std_logic,
-        reg_dst       : std_logic_vector (1 downto 0),
-        alu_op        : std_logic_vector (1 downto 0),
-        alu_src_a     : std_logic,
-        alu_src_b     : std_logic_vector (1 downto 0),
+    procedure assert_mem_access(
+        pc_write_cond : std_logic;
+        bne_cond      : std_logic;
+        pc_write      : std_logic;
+        i_or_d        : std_logic;
+        mem_read      : std_logic;
+        mem_write     : std_logic;
+        mem_to_reg    : std_logic_vector (1 downto 0);
+        ir_write      : std_logic;
+        reg_write     : std_logic;
+        reg_dst       : std_logic_vector (1 downto 0);
+        alu_op        : std_logic_vector (1 downto 0);
+        alu_src_a     : std_logic;
+        alu_src_b     : std_logic_vector (1 downto 0);
         pc_source     : std_logic_vector (1 downto 0)
-    ) return boolean is
+    )  is
     begin
         assert pc_write_cond = DEASSERTED report "pc_write_cond mem access state error";
         assert bne_cond      = DEASSERTED report "bne_cond      mem access state error";
@@ -170,24 +202,24 @@ begin
         assert alu_src_a     = DEASSERTED report "alu_src_a     mem access state error";
         assert alu_src_b     = B_ALU_SRC_B report "alu_src_b     mem access state error";
         assert pc_source     = ALU_RESULT_PC_SOURCE report "pc_source     mem access state error";
-    end assert_mem_access;
+    end procedure assert_mem_access;
     
-    function assert_reg_write
-        pc_write_cond : std_logic,
-        bne_cond      : std_logic,
-        pc_write      : std_logic,
-        i_or_d        : std_logic,
-        mem_read      : std_logic,
-        mem_write     : std_logic,
-        mem_to_reg    : std_logic_vector (1 downto 0),
-        ir_write      : std_logic,
-        reg_write     : std_logic,
-        reg_dst       : std_logic_vector (1 downto 0),
-        alu_op        : std_logic_vector (1 downto 0),
-        alu_src_a     : std_logic,
-        alu_src_b     : std_logic_vector (1 downto 0),
+    procedure assert_reg_write(
+        pc_write_cond : std_logic;
+        bne_cond      : std_logic;
+        pc_write      : std_logic;
+        i_or_d        : std_logic;
+        mem_read      : std_logic;
+        mem_write     : std_logic;
+        mem_to_reg    : std_logic_vector (1 downto 0);
+        ir_write      : std_logic;
+        reg_write     : std_logic;
+        reg_dst       : std_logic_vector (1 downto 0);
+        alu_op        : std_logic_vector (1 downto 0);
+        alu_src_a     : std_logic;
+        alu_src_b     : std_logic_vector (1 downto 0);
         pc_source     : std_logic_vector (1 downto 0)
-    ) return boolean is
+    )  is
     begin
         assert pc_write_cond = DEASSERTED report "pc_write_cond mem read state error";
         assert bne_cond      = DEASSERTED report "bne_cond      mem read state error";
@@ -203,23 +235,30 @@ begin
         assert alu_src_a     = DEASSERTED report "alu_src_a     mem read state error";
         assert alu_src_b     = B_ALU_SRC_B report "alu_src_b     mem read state error";
         assert pc_source     = ALU_RESULT_PC_SOURCE report "pc_source     mem read state error";
-    end assert_reg_write;
+    end procedure assert_reg_write;
     
     
     begin
-        cun : control_unit port map (clk => t_clk,
-                                    op_code => t_op_code);
-
         -- LW
 
         -- INSTRUCTION FETCH
         t_clk <= '0';
         wait for 1 ps;
         t_clk <= '1';
-        assert_inst_fetch(pc_write_cond, bne_cond, pc_write     
-                          i_or_d, mem_read, mem_write, mem_to_reg   
-                          ir_write, reg_write, reg_dst, alu_op  
-                          alu_src_a, alu_src_b, pc_source);
+        assert_inst_fetch(pc_write_cond => t_pc_write_cond,
+                          bne_cond      => t_bne_cond,
+                          pc_write      => t_pc_write,
+                          i_or_d        => t_i_or_d,
+                          mem_read      => t_mem_read,
+                          mem_write     => t_mem_write,
+                          mem_to_reg    => t_mem_to_reg,
+                          ir_write      => t_ir_write,
+                          reg_write     => t_reg_write,
+                          reg_dst       => t_reg_dst,
+                          alu_op        => t_alu_op,
+                          alu_src_a     => t_alu_src_a,
+                          alu_src_b     => t_alu_src_b,
+                          pc_source     => t_pc_source);
         t_op_code <= LW;
         wait for 1 ps;
         t_clk <= '0';
@@ -227,42 +266,86 @@ begin
         -- REGISTER READ / INST DECODE
         wait for 1 ps;
         t_clk <= '1';
-        assert_reg_read_inst_decode(pc_write_cond, bne_cond, pc_write     
-                          i_or_d, mem_read, mem_write, mem_to_reg   
-                          ir_write, reg_write, reg_dst, alu_op  
-                          alu_src_a, alu_src_b, pc_source);
+        assert_reg_read_inst_decode(pc_write_cond => t_pc_write_cond,
+                                    bne_cond      => t_bne_cond,
+                                    pc_write      => t_pc_write,
+                                    i_or_d        => t_i_or_d,
+                                    mem_read      => t_mem_read,
+                                    mem_write     => t_mem_write,
+                                    mem_to_reg    => t_mem_to_reg,
+                                    ir_write      => t_ir_write,
+                                    reg_write     => t_reg_write,
+                                    reg_dst       => t_reg_dst,
+                                    alu_op        => t_alu_op,
+                                    alu_src_a     => t_alu_src_a,
+                                    alu_src_b     => t_alu_src_b,
+                                    pc_source     => t_pc_source);
         wait for 1 ps;
         t_clk <= '0';
 
         -- ADDRESS CALCULATION
         wait for 1 ps;
         t_clk <= '1';
-        assert_addr_calc(pc_write_cond, bne_cond, pc_write     
-                          i_or_d, mem_read, mem_write, mem_to_reg   
-                          ir_write, reg_write, reg_dst, alu_op  
-                          alu_src_a, alu_src_b, pc_source);
+        assert_addr_calc(pc_write_cond => t_pc_write_cond,
+                         bne_cond      => t_bne_cond,
+                         pc_write      => t_pc_write,
+                         i_or_d        => t_i_or_d,
+                         mem_read      => t_mem_read,
+                         mem_write     => t_mem_write,
+                         mem_to_reg    => t_mem_to_reg,
+                         ir_write      => t_ir_write,
+                         reg_write     => t_reg_write,
+                         reg_dst       => t_reg_dst,
+                         alu_op        => t_alu_op,
+                         alu_src_a     => t_alu_src_a,
+                         alu_src_b     => t_alu_src_b,
+                         pc_source     => t_pc_source);
         wait for 1 ps;
         t_clk <= '0';
 
         -- MEM ACCESS
         wait for 1 ps;
         t_clk <= '1';
-        assert_mem_access(pc_write_cond, bne_cond, pc_write     
-                          i_or_d, mem_read, mem_write, mem_to_reg   
-                          ir_write, reg_write, reg_dst, alu_op  
-                          alu_src_a, alu_src_b, pc_source);
+        assert_mem_access(pc_write_cond => t_pc_write_cond,
+                          bne_cond      => t_bne_cond,
+                          pc_write      => t_pc_write,
+                          i_or_d        => t_i_or_d,
+                          mem_read      => t_mem_read,
+                          mem_write     => t_mem_write,
+                          mem_to_reg    => t_mem_to_reg,
+                          ir_write      => t_ir_write,
+                          reg_write     => t_reg_write,
+                          reg_dst       => t_reg_dst,
+                          alu_op        => t_alu_op,
+                          alu_src_a     => t_alu_src_a,
+                          alu_src_b     => t_alu_src_b,
+                          pc_source     => t_pc_source);
         wait for 1 ps;
         t_clk <= '0';
 
         -- REGISTER WRITE
         wait for 1 ps;
         t_clk <= '1';
-        assert_reg_write(pc_write_cond, bne_cond, pc_write     
-                          i_or_d, mem_read, mem_write, mem_to_reg   
-                          ir_write, reg_write, reg_dst, alu_op  
-                          alu_src_a, alu_src_b, pc_source);
+        assert_reg_write(pc_write_cond => t_pc_write_cond,
+                         bne_cond      => t_bne_cond,
+                         pc_write      => t_pc_write,
+                         i_or_d        => t_i_or_d,
+                         mem_read      => t_mem_read,
+                         mem_write     => t_mem_write,
+                         mem_to_reg    => t_mem_to_reg,
+                         ir_write      => t_ir_write,
+                         reg_write     => t_reg_write,
+                         reg_dst       => t_reg_dst,
+                         alu_op        => t_alu_op,
+                         alu_src_a     => t_alu_src_a,
+                         alu_src_b     => t_alu_src_b,
+                         pc_source     => t_pc_source);
         wait for 1 ps;
         t_clk <= '0';
+        
+        wait for 1 ps;
+        assert False report "Test end";
+        wait;
     end process;
 
-end behav
+end behav;
