aba06b52f9f672b1f901704bc237a0c14d440e56
ramirezjpdf <ramirez.jpdf@ymail.com>
2016-04-29 11:29:31 -0300

    res_aux is variable not signal

--- src/alu.vhd
+++ src/alu.vhd
@@ -17,41 +17,40 @@ entity alu is
 end alu;
 
 architecture behav of alu is
-
-signal res_aux : std_logic_vector (31 downto 0);
 begin
 	process(a, b, alu_control_in, shamt)
+	variable res_aux : std_logic_vector(31 downto 0);
 	begin
 		case alu_control_in is
-		when AND_CONTROL => res_aux <= a and b;
-		when OR_CONTROL  => res_aux <= a or b;
-		when NOR_CONTROL => res_aux <= not a or b;
+		when AND_CONTROL => res_aux := a and b;
+		when OR_CONTROL  => res_aux := a or b;
+		when NOR_CONTROL => res_aux := not a or b;
 		when SLT_CONTROL => 
 			if signed(a) < signed(b) then
-				res_aux <= x"00000001";
+				res_aux := x"00000001";
 			else
-				res_aux <= x"00000000";
+				res_aux := x"00000000";
 			end if;
-		when ADD_CONTROL => res_aux <=
+		when ADD_CONTROL => res_aux :=
 				     std_logic_vector(signed(a) + signed(b));
-		when SUB_CONTROL => res_aux <=
+		when SUB_CONTROL => res_aux :=
 				     std_logic_vector(signed(a) - signed(b));
-		when SLL_CONTROL => res_aux <=
+		when SLL_CONTROL => res_aux :=
 			     	     std_logic_vector(
                                      signed(b) sll to_integer(unsigned(shamt))
                                      );
-		when SRL_CONTROL => res_aux <=
+		when SRL_CONTROL => res_aux :=
 				     std_logic_vector(
                                      signed(b) srl to_integer(unsigned(shamt))
                                      );
-		when others => res_aux <= "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+		when others => res_aux := "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 		end case;
+
+		result <= res_aux;
 
 		case res_aux is
 			when x"00000000" => zero <= '1';
 			when others => zero <= '0';
 		end case;
-
-		result <= res_aux;
 	end process;
 end behav;
