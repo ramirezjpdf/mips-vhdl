efead32bd78d900ae128b5d57f656834aef4d514
ramirezjpdf <ramirez.jpdf@ymail.com>
2016-04-30 18:15:09 -0300

    add reg aux and reg special

---
+++ src/reg_aux.vhd
@@ -0,0 +1,25 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+use work.const.all;
+
+
+entity reg_aux is
+    port(clk : in std_logic;
+         in_data : in std_logic_vector(31 downto 0);
+         out_data: out std_logic_vector (31 downto 0));
+end reg_aux;
+
+architecture behav of reg_aux is
+signal reg : std_logic_vector(31 downto 0) := x"00000000";
+begin
+    process(clk)
+    begin
+        out_data <= reg;
+        if falling_edge(clk) then
+            reg <= in_data;
+            out_data <= in_data;
+        end if;
+    end process;
+end behav;
---
+++ src/reg_special.vhd
@@ -0,0 +1,26 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+use work.const.all;
+
+
+entity reg_special is
+    port(clk : in std_logic;
+         write_signal : in std_logic;
+         in_data : in std_logic_vector(31 downto 0);
+         out_data: out std_logic_vector (31 downto 0));
+end reg_special;
+
+architecture behav of reg_special is
+signal reg : std_logic_vector(31 downto 0) := x"00000000";
+begin
+    process(clk)
+    begin
+        out_data <= reg;
+        if falling_edge(clk) and write_signal = '1' then
+            reg <= in_data;
+            out_data <= in_data;
+        end if;
+    end process;
+end behav;
