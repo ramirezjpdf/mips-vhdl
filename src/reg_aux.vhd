ac5df0fa2eded03952c2345f6d62f3a4e482b2e4
ramirezjpdf <ramirez.jpdf@ymail.com>
2016-05-02 20:35:27 -0300

    add generic for choosing between falling and rising edge

--- src/reg_aux.vhd
+++ src/reg_aux.vhd
@@ -3,6 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity reg_aux is
+    generic(is_rising_edge : boolean := True);
     port(clk : in std_logic;
          in_data : in std_logic_vector(31 downto 0);
          out_data: out std_logic_vector (31 downto 0));
@@ -14,9 +15,16 @@ begin
     process(clk)
     begin
         out_data <= reg;
-        if falling_edge(clk) then
-            reg <= in_data;
-            out_data <= in_data;
+        if is_rising_edge then
+            if rising_edge(clk) then
+                reg <= in_data;
+                out_data <= in_data;
+            end if;
+        else    
+            if falling_edge(clk) then
+                reg <= in_data;
+                out_data <= in_data;
+            end if;
         end if;
     end process;
 end behav;
